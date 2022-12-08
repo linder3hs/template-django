a8a83b41eee8435131b22432544b00103dbdee79
linder3hs <linderhassinger00@gmail.com>
2022-12-08 02:47:37 -0500

    config db

--- school/settings.py
+++ school/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'celery'
 ]
 
-CSRF_TRUSTED_ORIGINS = ['https://template-django-production-6e65.up.railway.app/']
+CSRF_TRUSTED_ORIGINS = ['https://template-django-production-6e65.up.railway.app']
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
